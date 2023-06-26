1e756e97dea01f667675afb74adb0a1d4c6bb929
yinchaogao <yinchaogao@deeproute.ai>
2023-06-26 11:48:00 +0800

    init commit

---
+++ src/main.ts
@@ -0,0 +1,8 @@
+import { Plugin } from 'obsidian'
+
+export default class ExMarkdown extends Plugin {
+	async onload() {
+		// this.registerMarkdownCodeBlockProcessor('csv', (source, el, ctx) => {
+		// })
+	}
+}
