f04bfc2ca1efdcf1c370ae97eab59109ee21ef62
yinchaogao <yinchaogao@deeproute.ai>
2023-06-26 12:25:26 +0800

    support dot

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { Plugin } from 'obsidian'
 
+import Viz from '@viz-js/viz'
+
 export default class ExMarkdown extends Plugin {
 	async onload() {
-		// this.registerMarkdownCodeBlockProcessor('csv', (source, el, ctx) => {
-		// })
+		this.registerMarkdownCodeBlockProcessor('dot', this.dotRender.bind(this))
+	}
+
+	async dotRender(source, el): Promise<void> {
+		try {
+			let viz = await Viz.instance()
+			el.appendChild(viz.renderSVGElement(source))
+		} catch (err) {
+			el.createEl('span', { text: err.message })
+		}
 	}
 }
