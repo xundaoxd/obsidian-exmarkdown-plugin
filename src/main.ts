11a7a5543181f379c70336c9e2e667e72ffdff33
yinchaogao <yinchaogao@deeproute.ai>
2023-07-03 10:25:08 +0800

    support marmap

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,13 @@
 import { Plugin, MarkdownPostProcessorContext } from 'obsidian'
 
 import Viz from '@viz-js/viz'
+import { Transformer } from 'markmap-lib';
+import { Markmap, loadCSS } from 'markmap-view'
 
 export default class ExMarkdown extends Plugin {
 	async onload() {
 		this.registerMarkdownCodeBlockProcessor('dot', this.dotRender.bind(this))
+		this.registerMarkdownCodeBlockProcessor('markmap', this.markmapRender.bind(this))
 	}
 
 	async dotRender(source: string, el: HTMLElement, _: MarkdownPostProcessorContext): Promise<void> {
@@ -14,5 +17,16 @@ export default class ExMarkdown extends Plugin {
 		} catch (err) {
 			el.createEl('span', { text: err.message })
 		}
+	}
+	async markmapRender(source: string, el: HTMLElement, _: MarkdownPostProcessorContext): Promise<void> {
+		let svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg')
+		el.appendChild(svg)
+		setImmediate(() => {
+			const transformer = new Transformer();
+			const { root, features } = transformer.transform(source);
+			const { styles } = transformer.getUsedAssets(features);
+			if (styles) loadCSS(styles);
+			Markmap.create(svg, undefined, root);
+		})
 	}
 }
